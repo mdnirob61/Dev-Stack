2a1f99b5ab35e3ae07063beed3e0fe562eef61f4
mdnirob61 <nirobstudy2004@gmail.com>
2026-09-11 01:39:10 +0600

    Stack

--- Dev Stack/src/App.tsx
+++ Dev Stack/src/App.tsx
@@ -1,12 +1,29 @@
+import { Suspense, useState } from 'react';
 import Banner from './components/Banner'
 import Navbar from './components/Navbar'
+import type { Technology } from './technology';
+import Technologies from './components/Technologies';
+
+
+const technologyFetch = async (): Promise<Technology[]> => {
+  const res = await fetch("/technology.json");
+  const data = await res.json();
+  return data;
+}
 
 function App() {
+  const [technologyPromise] = useState(() => technologyFetch());
+  // console.log(technologyPromise);
 
   return (
     <>
       <Navbar></Navbar>
       <Banner></Banner>
+      <Suspense fallback={<p>Loading technologies...</p>}>
+        <Technologies
+          technologyPromise={technologyPromise}
+        />
+      </Suspense>
     </>
   )
 }
---
+++ Dev Stack/src/components/Technologies.tsx
@@ -0,0 +1,67 @@
+import React, { use } from 'react';
+import type { Technology } from '../technology';
+
+interface TechnologiesProps {
+    technologyPromise: Promise<Technology[]>
+}
+
+const Technologies = ({ technologyPromise }: TechnologiesProps) => {
+
+    const technologies = use(technologyPromise);
+    console.log(technologies);
+
+    return (
+        <div className="container mx-auto max-w-[1300px] px-5 sm:px-8 lg:px-10 mt-16">
+            <div className="mb-8">
+                <h2 className="text-3xl font-bold">
+                    Explore the <span className="text-transparent bg-clip-text bg-gradient-to-r from-red-500 to-purple-600">Technologies</span>
+                </h2>
+                <p className="text-gray-500 mt-2">
+                    Pick one technology per category to build your ideal stack.
+                </p>
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                {technologies.map((technology) => (
+                    <div
+                        key={technology.id}
+                        className="border border-gray-200 rounded-xl p-5 shadow-sm">
+                        <div className="flex items-center justify-between">
+                            <img
+                                src={technology.icon}
+                                alt={technology.name}
+                                className="w-7 h-7" />
+                            <div className="text-xs px-2 py-1 rounded-full bg-blue-50 text-blue-500">
+                                {technology.badge}
+                            </div>
+                        </div>
+                        <h3 className="text-2xl font-bold mt-4 text-[#e94b6a]">
+                            {technology.name}
+                        </h3>
+                        <p className="text-gray-500 mt-2">
+                            {technology.description}
+                        </p>
+                        <div className="flex items-center justify-between mt-5 text-xs">
+                            <div className="text-[1rem] bg-gray-200 px-2 py-1 rounded">
+                                {technology.category}
+                            </div>
+
+                            <div className="text-[1rem] text-gray-500">
+                                {technology.difficulty}
+                            </div>
+                            <div className="text-[1rem] text-gray-600">
+                                ⭐ {technology.rating}
+                            </div>
+                        </div>
+                        <button
+                            className="w-full mt-4 bg-[#d45b36] text-white text-[1rem] py-3 rounded-md hover:bg-[#fd683b] transition"
+                        >
+                            Add to Stack
+                        </button>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Technologies;
